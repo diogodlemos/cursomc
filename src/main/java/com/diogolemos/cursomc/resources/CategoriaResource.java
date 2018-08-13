be1ee9221eabc68793d242e050642701031e0e0a
diogodlemos <diogodrummond@hotmail.com>
2018-08-13 09:21:11 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/diogolemos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/diogolemos/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,8 @@
 package com.diogolemos.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+
+//import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
