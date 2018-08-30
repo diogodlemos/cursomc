34823b886252deebc883db6e31bbee7e32a206e6
diogodlemos <diogodrummond@hotmail.com>
2018-08-29 21:31:16 -0300

    Endpoint /pedidos / {id} disponibilizado

--- src/main/java/com/diogolemos/cursomc/domain/Cliente.java
+++ src/main/java/com/diogolemos/cursomc/domain/Cliente.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.diogolemos.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity 
 public class Cliente implements Serializable {
@@ -38,6 +39,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
